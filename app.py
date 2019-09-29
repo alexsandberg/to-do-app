b09f7876036365971ac140abf5225a77336de0aa
Alex Sandberg <alexsandberg7@gmail.com>
2019-09-29 10:01:03 -0600

    Implement try-except-finally for error handling

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://alex@localhost:5432/todoapp'
@@ -20,21 +21,44 @@ db.create_all()
 # POST request listener from form element
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    # gets value of key='description' from returning json
-    description = request.get_json()['description']
+    # define error flag
+    error = False
 
-    # create new Todo with form data
-    todo = Todo(description=description)
+    body = {}
 
-    # add new todo object to session and commit
-    # this adds new entry to database
-    db.session.add(todo)
-    db.session.commit()
+    # use try-except-finally to handle possible commit failure
+    try:
+        # gets value of key='description' from returning json
+        description = request.get_json()['description']
 
-    # return json using new description information
-    return jsonify({
-        'description': todo.description
-    })
+        # create new Todo with form data
+        todo = Todo(description=description)
+
+        # add new todo object to session and commit
+        # this adds new entry to database
+        db.session.add(todo)
+        db.session.commit()
+
+        # define body with description before session closes
+        body['description'] = todo.description
+    except:
+        # set error to T if error
+        error = True
+
+        # roll back session if commit fails
+        db.session.rollback()
+
+        # print error information
+        print(sys.exc_info())
+    finally:
+        # always close the session
+        db.session.close()
+    if error:
+        # abort request using code for Internal Server Error
+        abort(400)
+    else:
+        # return json using new description information
+        return jsonify(body)
 
 
 @app.route('/')

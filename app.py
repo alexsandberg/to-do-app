46353073523a68aece4fbab9a9c129b5299d8f71
Alex Sandberg <alexsandberg7@gmail.com>
2019-10-02 17:01:20 -0600

    Add TodoList to view

--- app.py
+++ app.py
@@ -97,7 +97,15 @@ def delete_todo(todo_id):
     return jsonify({'success': True})
 
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    # tells the view to render with index.html using latest model
+    return render_template('index.html',
+                           lists=TodoList.query.all(),
+                           active_list=TodoList.query.get(list_id),
+                           todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
+
 @app.route('/')
 def index():
-    # tells the view to render with index.html using latest model
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
